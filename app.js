c7ae4166c871e8782039f3be665e0533bde5c94b
Ramon Oestreich <ramon.oestreich@sistemafiergs.com.br>
2018-08-31 09:51:28 -0300

    commit initial

---
+++ app.js
@@ -0,0 +1,9 @@
+const os = require('os');
+
+console.log(os.platform())
+console.log(os.arch())
+console.log(os.cpus())
+console.log(os.type())
+console.log(os.release())
+console.log(os.freemem())
+console.log(os.totalmem())
